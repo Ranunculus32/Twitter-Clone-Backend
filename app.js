999bbf0070686ecc05fe013394c5f1fc075ee912
Christine Schale <108648777+Ranunculus32@users.noreply.github.com>
2024-04-17 11:07:00 +0200

    Database and local connection
    install packages: session, connect-mongodb-session
    
    create mongodb connection, session cookies, and port

---
+++ app.js
@@ -0,0 +1,37 @@
+const mongoose = require ("mongoose");
+const express = require ("express");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const dotenv = require("dotenv");
+dotenv.config();
+const port = 8000;
+
+
+// Middleware
+app.use(express.json());
+
+// Session and Flash Middleware
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URL,
+  collection: "sessions",
+});
+
+app.use(
+  session({
+    secret: process.env.secretKey,
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+  })
+);
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => console.log(err));
+
+// Start the server
+app.listen(port, () =>
+  console.log(`Backend server is running on port ${port}!`)
+);
