c7ad408c788f96147da755b90bef850646e6c46a
NnatasaK <natasakolaric2@gmail.com>
2024-05-22 01:54:27 +0200

    final changes

--- middleware/tweet_middleware.js
+++ middleware/tweet_middleware.js
@@ -1,8 +1,19 @@
 import tweetModel from "../models/tweet_model.js";
 import User from "../models/user_model.js";
+import Comment from "../models/Comment.js";
 
 
 // Controller function to create a new tweet
+export const randomImage = async (req, res) => {
+  try {
+    const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching random dog image:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
 export const createTweet = async (req, res) => {
   try {
 
@@ -59,16 +70,18 @@ export const getAllPosts = async (req, res) => {
 // Controller function to get a post by ID
 export const getPostById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const tweet = await tweetModel.findById(id);
+    const { postId } = req.params;
 
-    if (!tweet) {
-      return res.status(404).json({ error: 'Tweet not found' });
-    }
+    const comments = await Comment.find({ postId, isReply: false })
+      .populate('userId', 'username fullName') // Populate userId with username and fullName
+      .populate({
+        path: 'reply',
+        populate: { path: 'userId', select: 'username fullName' }
+      });
 
-    res.status(200).json(tweet);
+    res.status(200).json(comments);
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching comments:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
--- routers/tweet_router.js
+++ routers/tweet_router.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { createTweet, getAllPosts, getPostById, getFollowingPosts, editById, deleteById } from '../middleware/tweet_middleware.js';
+import { createTweet, getAllPosts, getPostById, getFollowingPosts, editById, deleteById, randomImage } from '../middleware/tweet_middleware.js';
 
 const router = express.Router();
 
 router.post('/create', createTweet);
+router.post('/random-dog-image', randomImage);
 router.get('/all', getAllPosts);
 router.get('/followingPosts/:userId', getFollowingPosts);
-router.get('/:id', getPostById);
+router.get('/:postId', getPostById);
 router.put('/:id', editById);
 router.delete('/:id', deleteById);
 
