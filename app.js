ab437aebcb938ddbdc02fab7d52dff60c82b7064
NnatasaK <natasakolaric2@gmail.com>
2024-05-23 18:21:02 +0200

    included another domain in cors

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const store = new MongoDBStoreSession({
 /* app.use(cors()); */
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://twitterx-clone-project.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
