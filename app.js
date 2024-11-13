642b63be673dc4f010bb5138437597f80cba9b5f
Meena M <meenasingh754@gmail.com>
2024-11-13 21:04:47 +0100

    revert back

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const store = new MongoDBStoreSession({
 /* app.use(cors()); */
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://x-twitter-clone-react.netlify.app/"],
+    origin: ["http://localhost:5173", "http://twitterx-clone-project.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
